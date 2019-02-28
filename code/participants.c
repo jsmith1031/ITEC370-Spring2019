61e489c9c7f22b8b11df23929e05188a5c2aa4b9
Amaiden1 <Amaiden1@radford.edu>
2019-02-27 22:56:50 -0500

    Added my name

--- code/participants.c
+++ code/participants.c
@@ -38,6 +38,7 @@ main (int argc, char *argv[])
   printf ("%02d: Joe Semendinger\n", i++);
 =======
   printf ("%02d: Justin Scyphers\n", i++);
+  printf ("%02d: Austin Maiden\n", i++);
 >>>>>>> 35969fb44994ce9b922914607205d4e6bc01028e
   printf ("There are %i participants.\n", i);
 
